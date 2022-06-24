bfdd24d6eb4d25e9db84782c23a073ee9a2dbfae
Zach <ztilos713@gmail.com>
2022-06-24 00:17:29 -0500

    Finished app basic functionality

--- index.js
+++ index.js
@@ -2,12 +2,22 @@
 
 
 let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
 
 let count = 0;
 
 
 function increment() {
-    count = count + 1; 
-    countEl.innerText = count;
-    console.log(count);
+    count += 1; 
+    countEl.textContent = count;
+
 }
+
+function save() {
+    let savedCount = `${count} - `;
+    saveEl.textContent += ` ${savedCount}`;
+    count = 0;
+    countEl.textContent = 0;
+
+}
+
